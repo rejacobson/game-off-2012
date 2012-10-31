93c5977886c881fe379be1219a39634ddc1e2444
Ryan Jacobson <rjacobson@bravenet.com>
2012-10-30 21:41:24 -0700

    Line making alternating right and left turns for the duration of it's lifespan

--- js/tree.js
+++ js/tree.js
@@ -9,8 +9,16 @@ vectors.distance_sq = function(a, b) {
   vectors.len_sq(vectors.subtract(a, b)); 
 };
 
-vectors.normal = function(v) {
+vectors.leftNormal = function(v) {
+  return [v[1], -v[0]];
+};
+
+vectors.rightNormal = function(v) {
   return [-v[1], v[0]];
+};
+
+vectors.toString = function(v) {
+  return v[0]+':'+v[1];
 };
 
 
@@ -47,7 +55,7 @@ console.log('new Lead() -- 1');
   
   var defaults = {
     lifespan: 5,
-    speed: 200,
+    speed: 30,
     step: 50,
     
     position: [0, 0],
@@ -59,7 +67,6 @@ console.log('new Lead() -- 1');
   }
   _.extend(this, defaults, settings);
 
-console.log(settings);
   this.heading(settings.destination);
 };
 
@@ -71,21 +78,42 @@ Lead.prototype.branch = function(settings) {
   this.tree.add_lead(settings);  
 };
 
+Lead.prototype.next_step = function() {
+  var new_direction;
+
+  if (this.lifespan % 2 == 0) {
+    new_direction = vectors.leftNormal(this.direction); 
+  } else {
+    new_direction = vectors.rightNormal(this.direction); 
+  }
+
+  var new_destination = vectors.add(this.destination, vectors.multiply(new_direction, this.step));
+  this.position = this.destination.slice(0);
+  this.heading(new_destination);
+};
+
 Lead.prototype.heading = function(destination) {
   this.destination = destination; // the destination of the line
   this.direction = vectors.unit(vectors.subtract(this.destination, this.position)); // unit direction of the heading
   this.velocity = vectors.multiply(this.direction, this.speed);
 };
 
-Lead.prototype.arrived = function() {
+Lead.prototype.has_arrived = function() {
   // Use dot product to determine what side of the destination the current position is on.
   // http://forums.anandtech.com/showthread.php?t=162930
   return vectors.dot(vectors.subtract(this.destination, this.position), this.direction) <= 0;
 };
 
 Lead.prototype.update = function(msDuration) {
+  if (this.lifespan <= 0) return;
+
   this.last_position = this.position.slice(0);
   this.position = vectors.add(this.position, vectors.multiply(this.velocity, msDuration));
+
+  if (this.has_arrived()) {
+    this.lifespan--;
+    this.next_step();
+  }
 };
 
 Lead.prototype.draw = function(display) {
