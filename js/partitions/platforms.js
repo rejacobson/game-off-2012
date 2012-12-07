2a9f89d2b93c7ce21e9022199ec1e92ea95f9fa9
Ryan Jacobson <rjacobson@bravenet.com>
2012-12-07 09:18:03 -0800

    Ground platforms render a ground and dirt tile; Ground platforms do not merge with tree branch platforms anymore

--- js/partitions/platforms.js
+++ js/partitions/platforms.js
@@ -1,6 +1,10 @@
 var gamejs = require('gamejs');
 var spatialpartition = require('spatialpartition');
 
+// Ground texture
+gamejs.preload(['images/ground.png']);
+
+
 var PlatformManager = exports.PlatformManager = function(height, cellheight) {
   spatialpartition.Grid.call(this, [1, height], [1, cellheight]);
 };
@@ -48,8 +52,10 @@ PlatformManager.prototype.mergeOverlapping = function(platform) {
   if (!platforms.length) return;
   
   _.each(platforms, function(p) {
-    platform.left = p.left = Math.min(p.left, platform.left);
-    platform.right = p.right = Math.max(p.right, platform.right);
+    if (platform.settings.fall_through == p.settings.fall_through) {
+      platform.left = p.left = Math.min(p.left, platform.left);
+      platform.right = p.right = Math.max(p.right, platform.right);
+    }
   });
 };
 
@@ -66,6 +72,7 @@ var Platform = exports.Platform = function(left, right, top, settings) {
   this.left = left;
   this.right = right;
   this.top = top;
+  this.height = 1;
 
   var defaults = {
     fall_through: true
@@ -78,7 +85,25 @@ Platform.prototype.width = function() {
   return this.right - this.left;
 };
 
+var ctx;
 Platform.prototype.draw = function(display) {
-  gamejs.draw.line(display, '#692B05', [this.left, this.top], [this.right, this.top], 1);
+  if (!this.settings.fall_through) {
+    if (!this.settings.pattern) {
+      var img = gamejs.image.load('images/ground.png');
+      this.height = img.getSize()[1];
+      this.settings.pattern = img.context.createPattern(img.canvas, 'repeat'); //display.context.createPattern(img.canvas, 'repeat');
+    }
+
+    ctx = display.context;
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle = this.settings.pattern;
+    ctx.translate(this.left, this.top);
+    ctx.fillRect(0, 0, this.right - this.left, this.height);
+    ctx.translate(-this.left, -this.top);
+    ctx.restore();
+  } else {
+    gamejs.draw.line(display, '#692B05', [this.left, this.top], [this.right, this.top], this.height);
+  }
 };
 
